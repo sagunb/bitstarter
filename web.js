417117ba2c910c467734879167da3941ec685fd6
Sagun B <sagun.b@gmail.com>
2013-07-19 23:15:15 +0000

    web.js now reads index.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+// declare modules
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var content;
+
 app.use(express.logger());
 
+//read index.html and modify buffer to string to send to client
+fs.readFileSync('index.html',function(err,data){
+	if (err) throw err;
+	content = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
