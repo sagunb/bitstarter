a74ad1f460f6eb863790a9a6359d9bb16189507d
Sagun B <sagun.b@gmail.com>
2013-07-20 23:41:18 +0000

    trying to add favicon

--- web.js
+++ web.js
@@ -13,8 +13,8 @@ app.get('/', function(request, response) {
   response.send(content.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
-//var port = process.env.PORT || 8080;
+//var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
