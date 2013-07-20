4d2a472504177957843f0f8bb81e0d7ed7c65ae0
Sagun B <sagun.b@gmail.com>
2013-07-20 07:04:20 +0000

    grader.sh finally works

--- grader.js
+++ grader.js
@@ -4,10 +4,24 @@ Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio.*/
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var checkurl = function(url){
+    rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error: ' + result.message);
+  } else {
+    var htmlfile = cheerio.load(result);
+    var checkJson = checkHtmlFile(htmlfile,program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
+});
+};
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -27,7 +41,7 @@ var loadChecks = function(checksfile){
 };
 
 var checkHtmlFile = function(htmlfile, checksfile){
-    $ = cheerioHtmlFile(htmlfile);
+    $ = htmlfile;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks){
@@ -46,10 +60,22 @@ if(require.main == module){
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-url, --url <url_link>', 'link to bitstarter website')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.url){
+    console.log("URL mode");
+console.log(program.url);
+    checkurl(program.url);
+ }
+    else{
+    console.log("File mode");
+console.log(program.file);
+    var content = fs.readFileSync(program.file);
+    var htmlfile = cheerio.load(content);
+    var checkJson = checkHtmlFile(htmlfile,program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
---
+++ score.sh
@@ -0,0 +1,2 @@
+./grader.js --checks checks.json --file index.html
+./grader.js --checks checks.json --url http://sagun.herokuapp.com
